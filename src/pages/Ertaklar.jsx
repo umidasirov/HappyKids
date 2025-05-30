107958902a6e7af6cce090fe48142ce7911ea28a
umidasirov <126274140+umidasirov@users.noreply.github.com>
2025-05-30 12:20:05 +0500

    Update Ertaklar.jsx

--- src/pages/Ertaklar.jsx
+++ src/pages/Ertaklar.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react"
 import { MainContext } from "../context/Context"
 import ErtaklarCard from "../components/Ertaklar/ErtaklarCard"
 export default function Ertaklar() {
-  const {ertaklar} = useContext(MainContext)
+  const {ertaklar,generateSlug} = useContext(MainContext)
   
     return (
           <div className="Ertaklar p-4">
@@ -15,7 +15,7 @@ export default function Ertaklar() {
               </nav>
               <div className="ertaklar">
                   {ertaklar.map((e)=>(
-                    <ErtaklarCard name={e.name} img={e.img} description={e.description} star={e.stars} slug={}/>
+                    <ErtaklarCard name={e.name} img={e.img} description={e.description} star={e.stars} slug={generateSlug(e.name)}/>
                   ))}
               </div>
           </div>
